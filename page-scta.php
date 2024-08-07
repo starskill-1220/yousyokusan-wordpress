e9c679f7cb71a057680cb750eee6a9a4636c2bf3
starskill-1220 <contact@star-skill.jp>
2024-08-08 01:33:29 +0900

    特定商取引法に基づく表記ページを追加完了

---
+++ page-scta.php
@@ -0,0 +1,87 @@
+<?php get_header(); ?>
+<main><section class="p-page-scta page-section" id="pageScta">
+  <div class="inner">
+    <div class="c-section-heading">
+      <h2>特定商取引法に基づく表記</h2>
+      <span>Specified Commercial Transaction Act</span>
+    </div>
+    <div class="p-page-scta__wrapper">
+      <ul class="p-page-scta__list">
+  <li class="p-page-scta__item">
+    <p class="p-page-scta__title">販売業者</p>
+    <p class="p-page-scta__body">養殖さん</p>
+  </li>
+  <li class="p-page-scta__item">
+    <p class="p-page-scta__title">運営統括責任者名</p>
+    <p class="p-page-scta__body">小野誠治</p>
+  </li>
+  <li class="p-page-scta__item">
+    <p class="p-page-scta__title">郵便番号</p>
+    <p class="p-page-scta__body">〒871-0311</p>
+  </li>
+  <li class="p-page-scta__item">
+    <p class="p-page-scta__title">住所</p>
+    <p class="p-page-scta__body">大分県中津市本耶馬渓町跡田1753</p>
+  </li>
+  <li class="p-page-scta__item">
+    <p class="p-page-scta__title">商品代金以外の料金の説明</p>
+    <p class="p-page-scta__body">
+      消費税込みの価格表示です。<br>
+      送料は梱包料を込みで、全国一律 ¥1,500です。<br>
+      ¥10,000以上お買い上げの場合、送料は無料と致します。<br>
+      手数料は<br class="sp">[クレジットカード：無料]<br class="sp">[代金引換：¥500]<br class="sp">[NP後払いwiz：¥250]
+    </p>
+  </li>
+  <li class="p-page-scta__item">
+    <p class="p-page-scta__title">申込有効期限</p>
+    <p class="p-page-scta__body">ご注文から７日以内と致します。</p>
+  </li>
+  <li class="p-page-scta__item">
+    <p class="p-page-scta__title">販売数量</p>
+    <p class="p-page-scta__body">商品ページにてご確認お願い致します。</p>
+  </li>
+  <li class="p-page-scta__item">
+    <p class="p-page-scta__title">引渡し時期</p>
+    <p class="p-page-scta__body">
+      生体につき、ご注文日から7日以内にお届けします。<br>
+      ご注文からお受け取りまで８日以上経過する注文はお受けできません。<br>
+      お振込みでのお支払の場合は、振込み確認後の発送となります。
+    </p>
+  </li>
+  <li class="p-page-scta__item">
+    <p class="p-page-scta__title">お支払い方法</p>
+    <p class="p-page-scta__body">
+      1.代金引換便（現金のお支払いが可能）<br>
+      2.クレジットカード支払い<br>
+      3.銀行振込（ゆうちょ銀行振込）
+    </p>
+  </li>
+  <li class="p-page-scta__item">
+    <p class="p-page-scta__title">お支払い期限</p>
+    <p class="p-page-scta__body">
+      ご注文確定日より7日以内<br>
+      期限を過ぎますと自動的にキャンセルとなります。
+    </p>
+  </li>
+  <li class="p-page-scta__item">
+    <p class="p-page-scta__title">サービス名</p>
+    <p class="p-page-scta__body">広島さつきセンター「めだかの館」</p>
+  </li>
+  <li class="p-page-scta__item">
+    <p class="p-page-scta__title">電話番号</p>
+    <p class="p-page-scta__body">080-2112-4245</p>
+  </li>
+  <li class="p-page-scta__item">
+    <p class="p-page-scta__title">公開メールアドレス</p>
+    <p class="p-page-scta__body">seiryuanmedaka@gmail.com</p>
+  </li>
+  <li class="p-page-scta__item">
+    <p class="p-page-scta__title">公開ホームページ</p>
+    <p class="p-page-scta__body">seiryuanmedaka@gmail.com</p>
+  </li>
+</ul>
+    </div>
+    <a href="<?php echo home_url("/"); ?>" class="c-btn">トップへ戻る</a>
+  </div>
+</section></main>
+<?php get_footer(); ?>
